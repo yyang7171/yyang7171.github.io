f7c374891234f2b3be32a759ca4a199db7847ccd
Yabing Yang <98364345+yyang7171@users.noreply.github.com>
2024-05-05 16:09:11 -0400

    annotations

--- script.js
+++ script.js
@@ -4,27 +4,34 @@ async function fetchData(url) {
   const data = await response.json();
   return data.data;
 }
+
+// Creates the artwork results
 function renderArtworks(artworks) {
   const resultsContainer = document.getElementById("results");
 
   artworks.forEach((artwork) => {
     const artworkElement = document.createElement("div");
     artworkElement.classList.add("artwork-item");
+
+    // In case the API gets artwork from the reading list
     let title = artwork.title.replace(" from Human_3.0 Reading List", "");
     let artistTitle = artwork.artist_title
       ? artwork.artist_title.replace(" from Human_3.0 Reading List", "")
       : "";
 
+    // Creating the titles
     const titleElement = document.createElement("h3");
     titleElement.textContent = title;
     artworkElement.appendChild(titleElement);
 
+    // And artist name
     if (artistTitle) {
       const artistTitleElement = document.createElement("p");
       artistTitleElement.textContent = artistTitle;
       artworkElement.appendChild(artistTitleElement);
     }
 
+    // Getting the image from the API using the provided URL with image ID
     if (artwork.image_id) {
       const imageElement = document.createElement("img");
       imageElement.src = `https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`;
@@ -32,13 +39,14 @@ function renderArtworks(artworks) {
       artworkElement.appendChild(imageElement);
     }
 
+    // Writing to the container
     resultsContainer.appendChild(artworkElement);
   });
 }
 
 // Initialize the app
 document.addEventListener("DOMContentLoaded", async () => {
-  // Check if the current page is artworks.html
+  // Check if the current page is artworks.html, then fetching from API
   if (window.location.pathname.includes("/artworks.html")) {
     const artworks = await fetchData(
       "https://api.artic.edu/api/v1/artworks?limit=100"
